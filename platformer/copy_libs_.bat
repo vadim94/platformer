a6ae4c7019ed1186c30c7bd32f7166f3e18e40b7
Ivan Petrenko <ioan.petrenko@gmail.com>
2017-05-20 15:54:45 +0200

    Updates paths for lib copy scripts

--- platformer/copy_libs_.bat
+++
@@ -1,7 +0,0 @@
-robocopy ..\..\platformer-deps\oxygine-framework\oxygine\third_party\win32\dlls ..\data libcurl.dll
-robocopy ..\..\platformer-deps\oxygine-framework\oxygine\third_party\win32\dlls ..\data pthreadVCE2.dll
-robocopy ..\..\platformer-deps\oxygine-framework\oxygine\third_party\win32\dlls ..\data zlib.dll
-robocopy ..\..\platformer-deps\oxygine-framework\libs ..\data SDL2.dll
-robocopy resources ..\data\resources *
-set rce=%errorlevel%
-if not %rce%==1 exit %rce% else exit 0
